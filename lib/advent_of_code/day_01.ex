b57cc65e56e157eeca9a7008d2a7e6f32efdb751
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-11-30 08:03:56 +0100

    Prepare Day1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,14 @@
 defmodule AdventOfCode.Day01 do
+  def parse(args) do
+    args |> String.split("\n", trim: true)
+  end
+
+  @spec part1(binary) :: [binary]
   def part1(args) do
+    parse(args)
   end
 
   def part2(args) do
+    parse(args)
   end
 end
