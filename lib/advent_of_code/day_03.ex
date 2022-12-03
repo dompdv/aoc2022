64910b4ad4bbbb485ac04bf2cdefa6003e095169
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-12-03 08:45:54 +0100

    Day3

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,32 @@
 defmodule AdventOfCode.Day03 do
+  import Enum
+
+  def priority(c) when c >= ?a, do: c - ?a + 1
+  def priority(c), do: c - ?A + 27
+
+  def intersect([]), do: []
+  def intersect([a, b]), do: MapSet.intersection(MapSet.new(a), MapSet.new(b)) |> MapSet.to_list()
+  def intersect([a | r]), do: intersect([a, intersect(r)])
+
+  def split_in_2(l), do: Enum.split(l, div(length(l), 2)) |> Tuple.to_list()
+
+  def parse_line(line) do
+    line |> String.trim() |> to_charlist() |> split_in_2() |> intersect() |> hd() |> priority()
+  end
+
   def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> map(&parse_line/1)
+    |> sum()
   end
 
   def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> map(fn l -> l |> String.trim() |> to_charlist() |> MapSet.new() end)
+    |> chunk_every(3)
+    |> map(fn g -> intersect(g) |> hd() |> priority() end)
+    |> sum()
   end
 end
