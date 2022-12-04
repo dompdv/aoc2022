9b8878d5be63ceb7f98e4830f4c4c88de5e0f0fe
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-12-04 12:04:38 +0100

    Day4

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,11 +1,22 @@
 defmodule AdventOfCode.Day04 do
   import Enum
 
-  def parse(args)
+  @mexpr ~r/(\d+)-(\d+),(\d+)-(\d+)/
 
-  def part1(args) do
+  def parse(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> map(fn line ->
+      Regex.run(@mexpr, line, capture: :all_but_first) |> map(&String.to_integer/1)
+    end)
   end
 
-  def part2(args) do
-  end
+  def contains([a, b, c, d]), do: (a <= c and b >= d) or (c <= a and d >= b)
+
+  def overlap([a, b, c, d]) when a > c, do: overlap([c, d, a, b])
+  def overlap([_a, b, c, _d]), do: b >= c
+
+  def part1(args), do: args |> parse() |> count(&contains/1)
+
+  def part2(args), do: args |> parse() |> count(&overlap/1)
 end
--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,15 @@
 defmodule AdventOfCode.Day05 do
+  import Enum
+
+  def parse(args) do
+    args
+  end
+
   def part1(args) do
+    args |> parse()
   end
 
   def part2(args) do
+    args |> parse()
   end
 end
