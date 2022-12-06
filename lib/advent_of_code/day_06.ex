2a84fecda09db15b68fd929df024e8d674ce19bd
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-12-06 09:02:10 +0100

    Day6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,12 @@
 defmodule AdventOfCode.Day06 do
-  def part1(args) do
+  import Enum
+
+  def start_mess([_ | r] = m, i, l) do
+    {p, _} = split(m, l)
+    if count(uniq(p)) == l, do: i + l, else: start_mess(r, i + 1, l)
   end
 
-  def part2(args) do
-  end
+  def part1(args), do: args |> to_charlist() |> start_mess(0, 4)
+
+  def part2(args), do: args |> to_charlist() |> start_mess(0, 14)
 end
