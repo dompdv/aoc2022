5c8fd681b51859fc596c5e34a4d64281aa961f7f
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-12-31 15:38:04 +0100

    Day16 p1 cleaned

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -59,84 +59,6 @@ defmodule AdventOfCode.Day16 do
     {distance, new_paths}
   end
 
-  def explore(30, current_pos, score, max_score, path, open_valves, _network, _distances, _paths) do
-    if :rand.uniform() > 0.999, do: IO.inspect({score, max_score, path}, label: "End")
-    score
-  end
-
-  def explore(time, current_pos, score, max_score, path, open_valves, network, distances, paths) do
-    #    IO.inspect({time, path}, label: "Explore")
-    # best case
-    max_additional_score =
-      for {to, d} <- distances[current_pos], not MapSet.member?(open_valves, d) do
-        Kernel.max(0, 30 - time - d - 1) * elem(network[to], 0)
-      end
-      |> sum()
-
-    # |> IO.inspect(label: "Explore>")
-
-    if score + max_additional_score < max_score do
-      # IO.inspect({time, score, max_score}, label: "stop")
-      nil
-    else
-      {current_valve, tos} = network[current_pos]
-
-      moves =
-        for(to <- tos, do: {:move, to})
-        |> sort_by(fn {_, to} ->
-          {to in paths, not MapSet.member?(open_valves, to), -elem(network[to], 0)}
-        end)
-
-      coups =
-        if(MapSet.member?(open_valves, current_pos) or current_valve == 0,
-          do: [],
-          else: [{:open, current_pos}]
-        ) ++ moves
-
-      reduce(coups, max_score, fn
-        {:open, pos} = shot, max_s ->
-          new_score =
-            explore(
-              time + 1,
-              pos,
-              score + (30 - time - 1) * current_valve,
-              max_s,
-              [shot | path],
-              MapSet.put(open_valves, pos),
-              network,
-              distances,
-              paths
-            )
-
-          cond do
-            new_score == nil -> max_s
-            new_score >= max_s -> new_score
-            true -> max_s
-          end
-
-        {:move, pos} = shot, max_s ->
-          new_score =
-            explore(
-              time + 1,
-              pos,
-              score,
-              max_s,
-              [shot | path],
-              open_valves,
-              network,
-              distances,
-              paths
-            )
-
-          cond do
-            new_score == nil -> max_s
-            new_score >= max_s -> new_score
-            true -> max_s
-          end
-      end)
-    end
-  end
-
   def bfs([], max_score, _), do: max_score
 
   def bfs(
@@ -162,7 +84,6 @@ defmodule AdventOfCode.Day16 do
         max_score,
         network
       ) do
-    # if :rand.uniform() > 0.99, do: IO.inspect(count(r), label: "BFS")
     {current_valve, tos} = network[current_pos]
 
     if current_score + rem_valves * (30 - time - 1) < max_score do
@@ -195,93 +116,51 @@ defmodule AdventOfCode.Day16 do
 
         if new_score > max_score, do: IO.inspect({new_score, new_state, count(r)})
 
-        bfs(sort_states([new_state | new_states] ++ r), new_max_score, network)
+        bfs([new_state | new_states] ++ r, new_max_score, network)
       else
-        bfs(sort_states(new_states ++ r), max_score, network)
+        bfs(new_states ++ r, max_score, network)
       end
     end
   end
 
-  def sort_states(l), do: l
-
-  def sort_states1(l) do
-    sort(
-      l,
-      fn %{score: score1, time: time1, rem_valves: rem_valves1},
-         %{score: score2, time: time2, rem_valves: rem_valves2} ->
-        #        score1 >= score2
-        #        rem_valves1 <= rem_valves2
-        time1 >= time2
-        # score1 + rem_valves1 * (30 - time1 - 1) >= score2 + rem_valves2 * (30 - time2 - 1)
-      end
-    )
-  end
-
-  def add_1_to_network(network) do
-    for {node, {v, nodes}} <- network, into: %{}, do: {node, {v, map(nodes, &{1, &1})}}
-  end
-
-  def part1(args) do
-    network = parse(args) |> IO.inspect()
-    total_valves = sum(for {_, {v, _}} <- network, do: v)
-
+  def reduce_network(network) do
     dist_paths =
       for n <- Map.keys(network),
           do: {n, populate_dist(n, network)}
 
-    paths = for {n, {_, p}} <- dist_paths, into: %{}, do: {n, p}
     distances = for {n, {d, _}} <- dist_paths, into: %{}, do: {n, d}
-    # IO.inspect(distances, label: "dist")
-    :ok
     nodes_with_valves = for {node, {v, _}} <- network, v > 0, do: node
 
-    reduced_network =
-      for node_from <- ["AA" | nodes_with_valves], into: %{} do
-        {node_from,
-         {network[node_from] |> elem(0),
-          for node_to <- nodes_with_valves, node_from != node_to do
-            {distances[node_from][node_to], node_to}
-          end}}
-      end
-
-    bfs(
-      [
-        %{
-          pos: "AA",
-          score: 0,
-          time: 0,
-          path: ["AA"],
-          open_valves: MapSet.new(),
-          rem_valves: total_valves
-        }
-      ],
-      0,
-      # add_1_to_network(network)
-      reduced_network
-    )
+    for node_from <- ["AA" | nodes_with_valves], into: %{} do
+      {node_from,
+       {network[node_from] |> elem(0),
+        for node_to <- nodes_with_valves, node_from != node_to do
+          {distances[node_from][node_to], node_to}
+        end}}
+    end
   end
 
-  def part1_old(args) do
-    network = parse(args)
+  def init_state(network) do
     total_valves = sum(for {_, {v, _}} <- network, do: v)
 
-    bfs(
-      [
-        %{
-          pos: "AA",
-          score: 0,
-          time: 0,
-          path: ["AA"],
-          open_valves: MapSet.new(),
-          rem_valves: total_valves
-        }
-      ],
-      0,
-      network
-    )
+    [
+      %{
+        pos: "AA",
+        score: 0,
+        time: 0,
+        path: ["AA"],
+        open_valves: MapSet.new(),
+        rem_valves: total_valves
+      }
+    ]
   end
 
-  def part1_very_old(args) do
+  def part1(args) do
+    network = parse(args) |> IO.inspect()
+    bfs(init_state(network), 0, reduce_network(network))
+  end
+
+  def part2(args) do
     args = """
     Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
     Valve BB has flow rate=13; tunnels lead to valves CC, AA
@@ -294,15 +173,5 @@ defmodule AdventOfCode.Day16 do
     Valve II has flow rate=0; tunnels lead to valves AA, JJ
     Valve JJ has flow rate=21; tunnel leads to valve II
     """
-
-    network = parse(args) |> IO.inspect()
-    dist_paths = for n <- Map.keys(network), do: {n, populate_dist(n, network)}
-    paths = for {n, {_, p}} <- dist_paths, into: %{}, do: {n, p}
-    distances = for {n, {d, _}} <- dist_paths, into: %{}, do: {n, d}
-
-    explore(0, "AA", 0, 0, [], MapSet.new(), network, distances, paths)
-  end
-
-  def part2(args) do
   end
 end
